838a5c896a7a98675f4981e51921e6bcbf7f731b
Ruichao Chen <65166963+Ruichaochen@users.noreply.github.com>
2026-01-15 22:43:42 +0000

    Create slime.lua

---
+++ Lua Scripts/Autofarms/slime.lua
@@ -0,0 +1,9 @@
+-- I think this the first for loop I'd ever written!
+_G.farm = true
+while _G.farm do
+while wait() do
+    game:GetService("Workspace").Ruichaochen.HumanoidRootPart.CFrame = game:GetService("Workspace").WildernessIsland.Entities.slime.HumanoidRootPart.CFrame
+    wait(0.001)
+    wait (0.001)
+end
+end
